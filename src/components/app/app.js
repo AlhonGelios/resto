c502ede6d4b90a32d29cf1795757eb968e71063f
Gelios <Geliosal39hon@gmail.com>
2021-04-26 12:37:00 +0300

    dz

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
+import {Switch, Route} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
     console.log(RestoService.getMenuItems());
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+            <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+                <AppHeader total={50}/>
+
+                <Switch>
+                    <Route path='/' exact component={MainPage}/>
+                    <Route path='/cart'  component={CartPage}/>
+                    <Route exact component={MainPage}/>
+                </Switch>
+            </div>
     )
 }
 
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,15 @@
 export default class RestoService {
-    getMenuItems() {
-        return [];
+    url = 'http://localhost:3000/menu';
+    
+    async getMenuItems () {
+        let res = await fetch(this.url);
+
+        if (res.ok) {
+            let json = await res.json();
+
+            return json;
+        } else {
+            throw new Error('Server Error');
+        }
     }
 }
